2eb8d284996c15ba8775b9d16f357723277cb5a9
karlorz <karl.logon@gmail.com>
2024-01-07 17:49:09 +0800

    skipDemoData: true

--- service/src/test/java/com/report/service/ServiceApplicationTests.java
+++ service/src/test/java/com/report/service/ServiceApplicationTests.java
@@ -2,20 +2,17 @@ package com.report.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
+@TestPropertySource(properties = {"skipDemoData=true"})
 @Slf4j
 class ServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 		log.info("begin test");
-		// Set the system property to skip the demo data insertion
-		System.setProperty("skipDemoData", "true");
-
-		// Unset or reset the system property after the test
-		System.clearProperty("skipDemoData");
+		// Your test logic here
 	}
-
 }
